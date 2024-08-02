f8a7b0f3bd102c7f5d78a243eef0114725c483fb
Alexandra Lee <alexandraelee9@gmail.com>
2024-08-02 13:07:04 +0000

    create database

---
+++ recipes/models.py
@@ -0,0 +1,28 @@
+from recipes import db
+
+# table for categories
+class Category(db.Model):
+    # schema for the Category model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    recipes = db.relationship("Recipe", backref="category", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.category_name
+
+#table for recipes
+class Recipe(db.Model):
+    # schema for the Recipe model
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(50), unique=True, nullable=False)
+    recipe_description = db.Column(db.Text, nullable=False)
+    recipe_type = db.Column(db.Text, nullable=False)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Recipe: {1} | Type: {2}".format(
+            self.id, self.recipe_name, self.recipe_type
+        )

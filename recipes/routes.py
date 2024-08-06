451047b34cf6997e63464e01da42d15afc6214d9
Alexandra Lee <alexandraelee9@gmail.com>
2024-08-06 14:57:13 +0000

    add categories

--- recipes/routes.py
+++ recipes/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from recipes import app, db
 from recipes.models import Category, Recipe
 
@@ -7,10 +7,17 @@ from recipes.models import Category, Recipe
 def home():
     return render_template("recipes.html")
 
+
 @app.route("/categories")
 def categories():
     return render_template("categories.html")
 
+
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
